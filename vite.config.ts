7d1bb9efeb5f1f66b0570964b1f3ede636a4c375
two8 <155454715+tacesept@users.noreply.github.com>
2025-11-24 21:54:24 +0630

    setup

---
+++ vite.config.ts
@@ -0,0 +1,34 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { dirname, resolve } from "path";
+import { fileURLToPath } from "url";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "#components": resolve(
+        dirname(fileURLToPath(import.meta.url)),
+        "components"
+      ),
+      "#constants": resolve(
+        dirname(fileURLToPath(import.meta.url)),
+        "constants"
+      ),
+      "#store": resolve(
+        dirname(fileURLToPath(import.meta.url)),
+        "store"
+      ),
+      "#hoc": resolve(
+        dirname(fileURLToPath(import.meta.url)),
+        "hoc"
+      ),
+      "#windows": resolve(
+        dirname(fileURLToPath(import.meta.url)),
+        "windows"
+      ),
+    },
+  },
+});
